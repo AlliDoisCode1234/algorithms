b7ded424661c1bee7b9228224bd561d56138787d
James Fowler <josiah.james.fowler@gmail.com>
2021-03-14 05:43:58 -0400

    vowelCounter

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -4,9 +4,51 @@ e.g vowelsCounter('anehizxcv') // will return 3
 */
 
 
-function vowelsCounter(text) {
-    // Code goes here
+// // _______Iterative Approach_______ // // 
+//
+//
+//
+
+// const vowels = ["a", "e", "i", "o", "u"]
+
+// function vowelsCounter(text) {  
+//     // Initialize counter
+//     let count = 0;
+
+//     // Loop through text to test if each char is a vowel
+//     for (let letter of text.toLowerCase()) {
+//         if(vowels.includes(letter)) {
+//             count++
+//         }
+//     }
+//     // return number of vowels
+//     return count
+// }
+
+
+
+
+// // _______RegEx Approach_______ // //
+//
+//
+//
+
+function vowelsCounter(text){
+    // Search text with Regex and store all matching instances
+    let matchingInstances = text.match(/[aeiou]/gi);
+
+    // check if matching instances exist then calculate length
+    if(matchingInstances){
+        // return number of vowels
+        return matchingInstances.length
+    } else{
+        return 0
+    }
 }
+
+
+
+
 
 
 
