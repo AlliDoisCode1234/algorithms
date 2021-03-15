88e976230d4355cf19a362672695062f3bfad86b
James Fowler <josiah.james.fowler@gmail.com>
2021-03-15 17:10:35 -0400

    map-object

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -5,9 +5,52 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 
 
+
+// function maxRecurringChar(text) {
+//     let charMap = {}
+//     let maxCharValue = 0
+//     let maxChar = ''
+
+//     for (let char of text){
+//         if (charMap.hasOwnProperty(char)){
+//             charMap[char]++
+//         } else {
+//             charMap[char] = 1
+//         }
+//     }
+
+//     for (let char in CharMap){
+//         if(charMap[char] > maxCharValue){
+//             maxCharValue = charMap[char]
+//             maxChar = char
+//         }
+//     }
+
+//     return maxChar
+// }
+
 function maxRecurringChar(text) {
-    // Code goes here
+    let charMap = {}
+    let charArray  = ['a', 'b', 'c','d']
+    let valuesArray = [4,3,2,1]
+    let maxCharValue = 0
+
+    for(let char of text){
+        if(charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else{
+            charMap[char] = 1
+        }
+    }
+
+    charArray = Object.keys(charMap)
+    valuesArray = Object.values(charMap)
+    maxCharValue = Math.max(...valuesArray)
+
+    return charArray[valuesArray.indexOf(maxCharValue)]
 }
+
+
 
 
 
