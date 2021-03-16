84b45bf89f9bb497f269ddbd16a30b8698b6da0b
James Fowler <josiah.james.fowler@gmail.com>
2021-03-16 03:55:15 -0400

    searchReplace

---
+++ Beginner/searchReplace/index-START.js
@@ -0,0 +1,29 @@
+// searchReplace("He is Sleeping on the couch", "Sleeping", "sitting") 
+// // should return "He is Sitting on the couch"
+
+// Using .replace()
+//
+//
+//
+
+// function searchReplace(str, word, newWord){
+//     if ( word[0] === word[0].toUpperCase() ) {
+//         newWord = newWord[0].toUpperCase() + newWord.slice(1)
+//    }
+//    return str.replace(word, newWord)
+// }
+
+
+// Using Regular Expressions
+//
+//
+//
+
+
+function searchReplace(str, word, newWord) {
+    let regex = new RegExp(word, 'gi');
+    if (/[A-Z]/.test(word[0])) {
+        newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1)
+    }
+    return str.replace(regex, newWord)
+}
