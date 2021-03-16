d72163dc295c47962790825bef6e0e093be8a55e
James Fowler <josiah.james.fowler@gmail.com>
2021-03-16 01:07:12 -0400

    trie

---
+++ Algos/Trie.js
@@ -0,0 +1,58 @@
+let Trie = function() {
+    
+    this.root = new Node()
+
+    this.add = function(input, node = this.root) {
+        if(input.length == 0) {
+            node.setEnd()
+            return;
+        } else if(!node.keys.has(input[0])) {
+            node.keys.set(input[0], new Node())
+            return this.add(input.substr(1), node.keys.get(input[0]))
+        } else {
+            node = node.keys.get(word[0])
+            word = word.substr(1)
+        }
+    }
+    return (node.keys.has(word) && node.keys.get(word).isEnd()) ?
+        true : false;
+}
+
+this.print = function() {
+    let words = new Array()
+    let search = function(node, string) {
+        if(node.keys.size != 0) {
+            for (let letter of node.keys.keys()) {
+                search(node.keys.get(letter), string.concat(letter))
+            }
+            if (node.isEnd()) {
+                words.push(string)
+            } else {
+                string.length > 0 ? words.push(string) : undefined;
+                return;
+            }
+        }
+        search(this.root, new String())
+        return words.length > 0 ? words : null
+    }
+}
+
+myTrie = new Trie()
+myTrie.add('ball')
+myTrie.add('bat')
+myTrie.add('bear')
+myTrie.add('doll')
+myTrie.add('dork')
+myTrie.add('do')
+myTrie.add('dorm')
+myTrie.add('send')
+myTrie.add('sense')
+myTrie.add('goal')
+myTrie.add('hurray')
+console.log(myTrie.isWord('doll'))
+console.log(myTrie.isWord('dor'))
+console.log(myTrie.isWord('dork'))
+console.log(myTrie.isWord('hurray'))
+console.log(myTrie.print())
+
+
